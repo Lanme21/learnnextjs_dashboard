74bb933e3f40a7345e6acab879fb44c2555c4be8
Lanme21 <alansuwandi02@gmail.com>
2025-03-28 15:51:36 +0700

    Revert "Test"

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import styles from "@/app/ui/home.module.css";
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
 
-
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
@@ -18,9 +17,9 @@ export default function Page() {
           <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course alan
+              Next.js Learn Course
             </a>
-            , brought to you by Vercel. alan
+            , brought to you by Vercel.
           </p>
           <Link
             href="/login"
